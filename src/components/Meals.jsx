db915f084e61125a065214f14e5af3fdedb1c18b
NelushiB <nelushib@hotmail.com>
2025-03-17 16:14:00 +0100

    Spostamento useeffect e gestione context

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import MealItem from './MealItem'
+import { useEffect, useState } from "react";
+import MealItem from "./MealItem";
+import { fetchAvailableMeals } from "../util/https";
 
-export default function Meals({meals}) {
+export default function Meals() {
+  const [availableMeals, setAvailableMeals] = useState([]);
+  const [error, setError] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const meals = await fetchAvailableMeals();
+        setAvailableMeals(meals);
+      } catch (error) {
+        setError({
+          message: error.message || "Failed to fetch available meals",
+        });
+      }
+    };
+
+    fetchMeals();
+  }, []);
+
   return (
-    <ul id='meals'>
-        {meals.map((meal) => (
-            <MealItem  key={meal.id} meal={meal}/>
-        ))}
+    <ul id="meals">
+      {availableMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
     </ul>
-  )
+  );
 }
