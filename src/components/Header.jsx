60861a5655cc94f1d7fa224abcbf84ce4f12e638
NelushiB <nelushib@hotmail.com>
2025-03-17 16:13:33 +0100

    Gestione totale carrello

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
+import { useContext } from 'react'
 import logo from '../assets/logo.jpg'
 import Button from './ui/Button'
+import CartContext from '../store/CartContext'
 
  export default function Header() {
+  const {items} = useContext(CartContext);
+
+  /* Funzione reduce
+  array.reduce((accumulatore, elementoCorrente(singolo oggetto dell'array)) => { ... }, valoreIniziale)
+  */
+  const totalCartItems = items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
   return (
     <header id='main-header'>
         <div id='title'>
@@ -9,7 +20,7 @@ import Button from './ui/Button'
             <h1>React food</h1>
         </div>
         <nav>
-            <Button textOnly>Cart (0)</Button>
+            <Button textOnly>Cart ({totalCartItems})</Button>
         </nav>
     </header>
   )
