5f74ab8e6ea3190e80d810af8f3776f00a2d2360
NelushiB <nelushib@hotmail.com>
2025-03-18 11:05:38 +0100

    Importazione context progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import { useContext } from 'react'
 import logo from '../assets/logo.jpg'
 import Button from './ui/Button'
 import CartContext from '../store/CartContext'
+import UserProgressContext from '../store/UserProgressContext';
 
  export default function Header() {
   const {items} = useContext(CartContext);
+  const { showCart } = useContext(UserProgressContext)
 
   /* Funzione reduce
   array.reduce((accumulatore, elementoCorrente(singolo oggetto dell'array)) => { ... }, valoreIniziale)
@@ -13,6 +15,10 @@ import CartContext from '../store/CartContext'
     return totalNumberOfItems + item.quantity;
   }, 0);
 
+  function handleShowCart(){
+    showCart();
+  }
+
   return (
     <header id='main-header'>
         <div id='title'>
@@ -20,7 +26,7 @@ import CartContext from '../store/CartContext'
             <h1>React food</h1>
         </div>
         <nav>
-            <Button textOnly>Cart ({totalCartItems})</Button>
+            <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
         </nav>
     </header>
   )
