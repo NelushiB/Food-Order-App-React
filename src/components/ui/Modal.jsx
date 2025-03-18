0dcbf095d2c8a50c4ef5c38e7dc6d1d6fa5bcced
NelushiB <nelushib@hotmail.com>
2025-03-18 11:05:16 +0100

    Creazione componente modal

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,6 +1,22 @@
-export default function Modal() {
-  return (
-    <dialog>Modal</dialog>
-  )
-}
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
 
+export default function Modal({ children, open, className }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    const modal = dialog.current;
+
+    if (open) {
+      modal.showModal();
+    }
+
+    // Cleanup function
+    return () => modal.close();
+  }, [open]);
+
+  return createPortal(
+    <dialog ref={dialog} className={`modal ${className}`}>{children}</dialog>,
+    document.getElementById("modal")
+  );
+}
